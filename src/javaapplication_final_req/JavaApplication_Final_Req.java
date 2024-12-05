a562a844335aaa07aa85eb0237c674b38393e9a5
Acer <Acer@192.168.6.62>
2024-12-05 15:16:12 +0800

    first commit!

---
+++ src/javaapplication_final_req/JavaApplication_Final_Req.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication_final_req;
+
+/**
+ *
+ * @author Acer
+ */
+public class JavaApplication_Final_Req {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
